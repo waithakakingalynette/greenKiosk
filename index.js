b668fd2283c6cd806fea2c19c1eeb007f366a7d7
waithakakingalynette <warukiralynette@gmail.com>
2023-05-22 19:51:57 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+document.getElementById('body').style.backgroundColor='silver'
+
+document.getElementById('title').style.color='green'
+document.getElementById('title').style.marginLeft='15%'
+
+document.getElementById('paragraph').style.marginLeft='20%'
+
+document.getElementById('titles').style.textTransform='uppercase'
+document.getElementById('titles').style.marginLeft='20%'
+document.getElementById('vegetables').style.textTransform='uppercase'
+document.getElementById('vegetables').style.marginLeft='20%'
+
+
+let fruit=document.createElement('li')
+fruit.innerHTML='Pawpaw'
+document.getElementById('fruits').appendChild(fruit)
+document.getElementById('fruits').style.marginLeft='20%'
+
+
+let x=document.createElement('li')
+x.innerHTML='Brocoli'
+document.getElementById('vegList').appendChild(x)
+document.getElementById('vegList').style.marginLeft='20%'
+
+// let y=document.createElement('img')
+// y.innerHTML='mango.jpg'
+// document.getElementById('fruits').appendChild(y)
+
+document.getElementById('banana').style.maxWidth='250px'
+document.getElementById('banana').style.maxHeight='250px'
+document.getElementById('banana').style.marginLeft='50px'
+
+
+document.getElementById('mango').style.maxWidth='250px'
+document.getElementById('mango').style.maxHeight='250px'
+document.getElementById('mango').style.marginLeft='100px'
+
+
+document.getElementById('melon').style.maxWidth='250px'
+document.getElementById('melon').style.maxHeight='250px'
+document.getElementById('melon').style.marginLeft='150px'
+
+
+document.getElementById('pawpaw').style.maxWidth='250px'
+document.getElementById('pawpaw').style.maxHeight='250px'
+document.getElementById('pawpaw').style.marginLeft='200px'
